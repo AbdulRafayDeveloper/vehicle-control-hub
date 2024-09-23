a1fe4f3994fe23864ad485ce85f0074eaa1c289d
Abdul Rafay <amirrafay135@gmail.com>
2024-09-24 00:58:48 +0500

    update code

--- src/core/routes/Routes.jsx
+++ src/core/routes/Routes.jsx
@@ -1,60 +1,34 @@
-import lazy from "../../views/components/LazyLoadWithRetry/LazyLoadWithRetry.jsx";
-import ContainerForm from "../../views/pages/Containers/ContainerForm.jsx";
-import CustomerForm from "../../views/pages/Customer/CustomerForm.jsx";
-import ItemForm from "../../views/pages/Inventory/ItemForm.jsx";
-import SparePartForm from "../../views/pages/SpareParts/SparePartForm.jsx";
-import VehicleForm from "../../views/pages/Vehicle/VehicleForm.jsx";
-const Home = lazy(() => import("../../views/pages/Home/Home"));
-const LoginForm = lazy(() => import("../../views/pages/Auth/Login"));
+import lazy from '../../views/components/LazyLoadWithRetry/LazyLoadWithRetry.jsx';
+import ContainerForm from '../../views/pages/Containers/ContainerForm.jsx';
+import CustomerForm from '../../views/pages/Customer/CustomerForm.jsx';
+import ItemForm from '../../views/pages/Inventory/ItemForm.jsx';
+import SparePartForm from '../../views/pages/SpareParts/SparePartForm.jsx';
+import VehicleForm from '../../views/pages/Vehicle/VehicleForm.jsx';
+const Home = lazy(() => import('../../views/pages/Home/Home'));
+const LoginForm = lazy(() => import('../../views/pages/Auth/Login'));
 const ForgotPassword = lazy(() =>
-  import("../../views/pages/Auth/ForgetPassword")
+  import('../../views/pages/Auth/ForgetPassword')
 );
-import Notifications from "../../views/pages/User/Notifications.jsx";
-import Dashboard from "../../views/pages/User/Dashboard.jsx";
-import WatchList from "../../views/pages/User/WatchList.jsx";
-import Favorites from "../../views/pages/User/Favourites.jsx";
-import Bids from "../../views/pages/User/Bids.jsx";
-import PersonalInfoForm from "../../views/pages/User/PersonalProfile/PersonalProfile.jsx";
 
-const ResetPassword = lazy(() => import("../../views/pages/Auth/RestPassword"));
-const RegisterForm = lazy(() => import("../../views/pages/Auth/Register.jsx"));
-const Admin = lazy(() => import("../../views/pages/Admin/AdminPortal.jsx"));
-const Auctions = lazy(() =>
-  import("../../views/pages/Auctions/LiveAuctions.jsx")
-);
-const AuctionItems = lazy(() =>
-  import("../../views/pages/Auctions/AuctionItems.jsx")
-);
-const SparePartListing = lazy(() =>
-  import("../../views/pages/SpareParts/SparePartListing.jsx")
-);
-const VehicleListing = lazy(() =>
-  import("../../views/pages/Vehicle/VehicleListing.jsx")
-);
-const Customer = lazy(() =>
-  import("../../views/pages/Customer/CustomerPortal.jsx")
-);
-const Employee = lazy(() =>
-  import("../../views/pages/Employee/EmployeePortal.jsx")
-);
-const UploadDocument = lazy(() =>
-  import("../../views/pages/BLDocuments/UploadDocument.jsx")
-);
-const Container = lazy(() =>
-  import("../../views/pages/Containers/ContainerListing.jsx")
-);
-const SalePurchase = lazy(() =>
-  import("../../views/pages/SalePurchase/SalePurchase.jsx")
-);
-const Inventory = lazy(() =>
-  import("../../views/pages/Inventory/Inventory.jsx")
-);
-const Item = lazy(() => import("../../views/pages/Inventory/Item.jsx"));
-const Blog = lazy(() => import("../../views/pages/Blog/Blog.jsx"));
+const ResetPassword = lazy(() => import('../../views/pages/Auth/RestPassword'));
+const RegisterForm = lazy(() => import('../../views/pages/Auth/Register.jsx'))
+const Admin = lazy(() => import('../../views/pages/Admin/AdminPortal.jsx'));
+const Auctions = lazy(() => import('../../views/pages/Auctions/LiveAuctions.jsx'));
+const AuctionItems = lazy(() => import('../../views/pages/Auctions/AuctionItems.jsx'));
+const SparePartListing = lazy(() => import('../../views/pages/SpareParts/SparePartListing.jsx'));
+const VehicleListing = lazy(() => import('../../views/pages/Vehicle/VehicleListing.jsx'));
+const Customer = lazy(() => import('../../views/pages/Customer/CustomerPortal.jsx'));
+const Employee = lazy(() => import('../../views/pages/Employee/EmployeePortal.jsx'));
+const UploadDocument = lazy(() => import('../../views/pages/BLDocuments/UploadDocument.jsx'));
+const Container = lazy(() => import('../../views/pages/Containers/ContainerListing.jsx'));
+const SalePurchase = lazy(() => import('../../views/pages/SalePurchase/SalePurchase.jsx'));
+const Inventory = lazy(() => import('../../views/pages/Inventory/Inventory.jsx'));
+const Item = lazy(() => import('../../views/pages/Inventory/Item.jsx'));
+const Blog = lazy(() => import('../../views/pages/Blog/Blog.jsx'));
 export const routes = [
   // All protected routes
   {
-    path: "/home",
+    path: '/home',
     isProtected: true,
     page: <Home />,
     aside: null,
@@ -64,188 +38,152 @@ export const routes = [
 
   // All public routes
   {
-    path: "/admin",
+    path:'/admin',
     page: <Admin />,
     isProtected: true,
     aside: null,
   },
   {
-    path: "/user/notifications",
-    page: <Notifications />,
-    isProtected: true,
-    aside: null,
-  },
-  {
-    path: "/user/dashboard",
-    page: <Dashboard />,
-    isProtected: true,
-    aside: null,
-  },
-  {
-    path: "/user/watch-list",
-    page: <WatchList />,
-    isProtected: true,
-    aside: null,
-  },
-  {
-    path: "/user/favorites",
-    page: <Favorites />,
-    isProtected: true,
-    aside: null,
-  },
-  {
-    path: "/user/bids",
-    page: <Bids />,
-    isProtected: true,
-    aside: null,
-  },
-  {
-    path: "/user/profile",
-    page: <PersonalInfoForm />,
-    isProtected: true,
-    aside: null,
-  },
-  {
-    path: "/auctions",
+    path: '/auctions',
     page: <Auctions />,
     isProtected: true,
     aside: null,
   },
 
   {
-    path: "/auctions/:category/:id",
+    path: '/auctions/:category/:id',
     page: <AuctionItems />,
     isProtected: true,
     aside: null,
   },
   {
-    path: "/spare-part-listing",
-    page: <SparePartListing />,
+    path: '/spare-part-listing',
+    page: <SparePartListing />, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/add-spare-part-listing",
-    page: <SparePartForm edit={false} />,
+    path: '/add-spare-part-listing',
+    page: <SparePartForm edit={false} />, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/edit-spare-part-listing/:id",
-    page: <SparePartForm edit={true} />,
+    path: '/edit-spare-part-listing/:id',
+    page: <SparePartForm edit={true} />, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/vehicle-listing",
-    page: <VehicleListing />,
+    path: '/vehicle-listing',
+    page: <VehicleListing />, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/add-vehicle-listing",
-    page: <VehicleForm edit={false} />,
+    path: '/add-vehicle-listing',
+    page: <VehicleForm edit={false} />, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/edit-vehicle-listing/:id",
-    page: <VehicleForm edit={true} />,
+    path: '/edit-vehicle-listing/:id',
+    page: <VehicleForm edit={true} />, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/customers",
+    path: '/customers',
     page: <Customer />,
     isProtected: true,
     aside: null,
   },
   {
-    path: "/add-customer",
+    path: '/add-customer',
     page: <CustomerForm edit={false} />,
     isProtected: true,
     aside: null,
   },
   {
-    path: "/edit-customer/:id",
-    page: <CustomerForm edit={true} />,
+    path: '/edit-customer/:id',
+    page: <CustomerForm edit={true}/>,
     isProtected: true,
     aside: null,
   },
   {
-    path: "/employee",
+    path: '/employee',
     page: <Employee />,
     isProtected: true,
     aside: null,
   },
 
   {
-    path: "/bl-documents",
+    path: '/bl-documents',
     isProtected: true,
     page: <UploadDocument />,
     aside: null,
   },
 
   {
-    path: "/containers",
+    path: '/containers',
     isProtected: true,
     page: <Container />,
     aside: null,
   },
   {
-    path: "/add-container",
-    page: <ContainerForm edit={false} />,
+    path: '/add-container',
+    page: <ContainerForm edit={false}/>, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/edit-container/:id",
-    page: <ContainerForm edit={true} />,
+    path: '/edit-container/:id',
+    page: <ContainerForm edit={true}/>, 
     isProtected: true,
     aside: null,
   },
 
   {
-    path: "/sale-purchase",
+    path: '/sale-purchase',
     isProtected: true,
     page: <SalePurchase />,
     aside: null,
   },
   {
-    path: "/inventory",
+    path: '/inventory',
     isProtected: true,
     page: <Inventory />,
     aside: null,
   },
 
   {
-    path: "/item",
+    path: '/item',
     isProtected: true,
     page: <Item />,
     aside: null,
   },
   {
-    path: "/edit-item/:id",
-    page: <ItemForm edit={true} />,
+    path: '/edit-item/:id',
+    page: <ItemForm edit={true}/>, 
     isProtected: true,
     aside: null,
   },
   {
-    path: "/add-item",
-    page: <ItemForm edit={false} />,
+    path: '/add-item',
+    page: <ItemForm edit={false}/>, 
     isProtected: true,
     aside: null,
   },
 
   {
-    path: "/blog",
+    path: '/blog',
     isProtected: true,
     page: <Blog />,
     aside: null,
   },
   /* auth routes */
   {
-    path: "/login",
+    path: '/login',
     page: <LoginForm />,
     isAuthRoute: true,
     // header: null,
@@ -253,7 +191,7 @@ export const routes = [
     aside: null,
   },
   {
-    path: "/register",
+    path: '/register',
     isAuthRoute: true,
     page: <RegisterForm />,
     // header: null,
@@ -261,7 +199,7 @@ export const routes = [
     aside: null,
   },
   {
-    path: "/forgot_password",
+    path: '/forgot_password',
     isAuthRoute: true,
     page: <ForgotPassword />,
     header: null,
@@ -270,7 +208,7 @@ export const routes = [
   },
 
   {
-    path: "/reset-password",
+    path: '/reset-password',
     isAuthRoute: true,
     page: <ResetPassword />,
     header: null,
